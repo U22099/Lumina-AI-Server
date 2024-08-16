7679db694654ba4179cfa4d3ac15896fb34ffb97
Daniel <u22099dandev@gmail.com>
2024-08-16 12:36:14 +0000

    Solving cors issue

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     "https://u22099.github.io", 
     "http://localhost:5173",
-    "https://https://2drl3r-5173.csb.app"
+    "https://2drl3r-5173.csb.app"
 ];
 
 const credentials = (req, res, next) => {
