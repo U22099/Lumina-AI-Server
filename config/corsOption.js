2f7b0b633bbad51903ad6bf808f40d2b314aed7a
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-27 00:58:26 +0100

    Add files via upload

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,7 +1,5 @@
 const allowedOrigin = [
     'https://u22099.github.io',
-	 'https://u22099.github.io/',
-	 'https://u22099.github.io/Lumina',
     'http://localhost:5173',
     'https://2drl3r-5173.csb.app',
     'https://silver-space-fortnight-pj9gjvj4v6qf6p9-5173.app.github.dev'
@@ -9,7 +7,8 @@ const allowedOrigin = [
 
 const corsOption = {
     origin: (origin, callback) => {
-        if(allowedOrigin.includes(origin) || !origin){
+console.log(origin);
+        if(true){
             callback(null, true);
         }else{
             callback(new Error('Not Allowed By CORS'));
