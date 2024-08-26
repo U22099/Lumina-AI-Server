435c53114dc407d0e1481706aa2acc5b3985c337
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-27 00:52:23 +0100

    Add files via upload

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,5 +1,7 @@
 const allowedOrigin = [
     'https://u22099.github.io',
+	 'https://u22099.github.io/',
+	 'https://u22099.github.io/Lumina',
     'http://localhost:5173',
     'https://2drl3r-5173.csb.app',
     'https://silver-space-fortnight-pj9gjvj4v6qf6p9-5173.app.github.dev'
