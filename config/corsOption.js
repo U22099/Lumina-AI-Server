6af6e17146bcc413d0a0e1d558d6ac9387255bad
Daniel <u22099dandev@gmail.com>
2024-10-01 05:38:43 +0000

    —-

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,6 +1,6 @@
 const allowedOrigin = [
     'https://u22099.github.io',
-    'http://localhost:5173',
+    "https://2drl3r-5173.csb.app",
     'https://qpx3lq-5173.csb.app',
     'https://silver-space-fortnight-pj9gjvj4v6qf6p9-5173.app.github.dev'
 ];
